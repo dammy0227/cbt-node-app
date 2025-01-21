9af2bf0382fd589ee4beecfa58035e35b5b406f8
dammy <fatunsindamilare1@gmail.com>
2025-01-21 08:19:02 +0100

    Fix Vercel deployment

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const examRoutes = require('./routes/examRoutes');
+const express = require("express");
+const cors = require("cors");
+const examRoutes = require("./routes/examRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/exam', examRoutes);
+// API Routes
+app.use("/api/exam", examRoutes);
 
-const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+// Export the app (for Vercel)
+module.exports = app;
